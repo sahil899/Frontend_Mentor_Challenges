cd30c00b116e7cc6a896e421fd8d77487a7fda16
sahil899 <sahilsharma2471@gmail.com>
2022-04-13 16:21:34 +0530

    advice generator app

---
+++ advice-generator-app-main/script.js
@@ -0,0 +1,17 @@
+const heading = document.querySelector(".heading");
+const text = document.querySelector(".text");
+const button = document.querySelector("button");
+
+function fetchData() {
+  fetch("https://api.adviceslip.com/advice")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      const id = data.slip.id;
+      const advice = data.slip.advice;
+      heading.textContent = `ADVICE #${id}`;
+      text.innerHTML = `&#8220 ${advice} &#8221`;
+    });
+}
+
+button.addEventListener("click", fetchData);
